196c086d0da7d253a363b761a0463238ea062d28
gassalin <gustavo.assalin@hotmail.com>
2026-09-09 22:39:37 -0300

    Ajustes e melhorias docker

--- ms-autenticacao/src/main/java/br/com/unicos/ms_autenticacao/config/JpaAuditingConfig.java
+++ ms-autenticacao/src/main/java/br/com/unicos/ms_autenticacao/config/JpaAuditingConfig.java
@@ -4,13 +4,11 @@ import br.com.unicos.ms_autenticacao.model.AuthenticatedUser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 
-@EnableJpaAuditing
 @Configuration
 public class JpaAuditingConfig {
 
